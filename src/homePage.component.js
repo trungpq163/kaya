ef39ec48ca35006500d559cab204ef9a28b2bb28
Quốc Trung <quoctrung163@QuocTrungs-MacBook-Pro.local>
2020-03-28 19:47:39 +0700

    fix scss :dophin:

--- src/homePage.component.js
+++ src/homePage.component.js
@@ -4,35 +4,34 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
     <div className='directory-menu'>
       <div className='menu-item'>
         <div className='content'>
-          <div className='title'>HATS</div>
+          <h1 className='title'>HATS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
       <div className='menu-item'>
         <div className='content'>
-          <div className='title'>JACKETS</div>
+          <h1 className='title'>JACKETS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
       <div className='menu-item'>
         <div className='content'>
-          <div className='title'>SHOES</div>
+          <h1 className='title'>SNEAKERS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
       <div className='menu-item'>
         <div className='content'>
-          <div className='title'>WOMENS</div>
+          <h1 className='title'>WOMENS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
       <div className='menu-item'>
         <div className='content'>
-          <div className='title'>MENS</div>
+          <h1 className='title'>MENS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
