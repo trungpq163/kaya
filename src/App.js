85fb760e4842bd1501c8f19954721fafde0ee1d6
Quốc Trung <quoctrung163@QuocTrungs-MacBook-Pro.local>
2020-03-28 17:34:39 +0700

    add sass, create our homepage component and add menu directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import HomePage from './homePage';
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <HomePage />
     );
   }
 }
