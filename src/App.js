2170dec98f5a2578c92a9e793d600fb848bf11a8
Quốc Trung <quoctrung163@icloud.com>
2020-03-30 14:46:59 +0700

    change directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 class App extends Component {
   render() {
