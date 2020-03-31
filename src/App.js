1b0f4c468d6004a2c5fe250c8f285f72506f8c7f
Quốc Trung <quoctrung163@icloud.com>
2020-03-31 20:07:48 +0700

    fix svg image and add header component and link to home, shop page and contact :shark:

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage}/>
--- webpack.common.js
+++ webpack.common.js
@@ -25,11 +25,15 @@ module.exports = {
       ],
       exclude: [/node_modules/]
     }, {
-      test: /\.(png|svg|jpg|gif)$/,
+      test: /\.(png|jpg|gif)$/,
       use: [
         'file-loader',
+        'url-loader'
       ],
       exclude: [/node_modules/]
+    }, {
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
     }]
   },
   performance: {
