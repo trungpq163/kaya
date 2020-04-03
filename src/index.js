d179f657b0c5869431b2c4f68e08db6de1ac8c0c
Quốc Trung <quoctrung163@icloud.com>
2020-04-03 07:07:28 +0700

    added redux, implemented userReducer and userAction to Header, App Component:cat: :dog:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 import './index.css';
 
@@ -9,8 +12,10 @@ import App from './App';
 const root = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   root
 );
