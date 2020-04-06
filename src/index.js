28a5d346c6b64fc0cb029fc070e2158b28d2ea70
Quốc Trung <quoctrung163@icloud.com>
2020-04-07 05:19:29 +0700

    add redux-persist for store data with localStorage :sunny:

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 
@@ -14,7 +15,9 @@ const root = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   root
