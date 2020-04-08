dce3be18f2e46c1c8881b2da726d32ebe3cf29b3
Quốc Trung <quoctrung163@icloud.com>
2020-04-08 13:44:02 +0700

    add styled-components, converted homepage :sunny: :fire:

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -8,23 +7,7 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = token => {
     console.log(token);
-    axios({
-      url: '/payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token
-      }
-    })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        console.log('Payment error: ', err);
-        alert(
-          'There was an issue with your payment! Please make sure to use the provided credit data cart'
-        );
-      });
+    alert('Payment Succesful!');
   };
 
   return (
