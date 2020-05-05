7dd3e226a5cb2636a6d0eb710d7f4cfb084257ac
Quốc Trung <quoctrung163@icloud.com>
2020-05-05 18:27:21 +0700

    add item-detail

---
+++ src/components/item-details/item-details.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import './item-details.styles.scss';
+
+const ItemDetail = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-lg-7">
+          <img src="https://i.picsum.photos/id/688/500/300.jpg" alt="IMG-PRODUCT" />
+        </div>
+        <div className="col-md-6 col-lg-5">
+          <div>
+            <h4 className='text'>
+              Lightweight Jacket
+            </h4>
+            <span>
+              $58.79
+            </span>
+            <h4 className='text'>
+              Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
+            </h4>
+            
+            <button>Add to cart</button>
+
+
+            <div className="social-icon">
+              <a href="#" className="icon" data-tooltip="Facebook">
+                <i className="fa fa-facebook" />
+              </a>
+              <a href="#" className="icon" data-tooltip="Twitter">
+                <i className="fa fa-twitter" />
+              </a>
+              <a href="#" className="icon" data-tooltip="Google Plus">
+                <i className="fa fa-google-plus" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemDetail;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,6 +6,8 @@ import CollectionPage from '../collection/collection.component';
 
 import ItemPage from '../item/item.component';
 
+import ItemDetail from '../../components/item-details/item-details.component';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,7 +15,7 @@ const ShopPage = ({ match }) => (
   <div className="shop-page container mt-5 pt-5">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
-    <Route exact path={`${match.path}/:collectionId/:itemId`} component={ItemPage} />
+    <Route exact path={`${match.path}/:collectionId/:itemId`} component={ItemDetail} />
     <ToastContainer />
   </div>
 );
