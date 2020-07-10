ef6f891079f7f06280178694df2c9ea72afcd277
Quốc Trung <quoctrung163@icloud.com>
2020-07-10 11:05:13 +0700

    add comment fb :heart:

--- src/components/comments/comments.component.jsx
+++ src/components/comments/comments.component.jsx
@@ -4,8 +4,8 @@ import { FacebookProvider, Comments } from 'react-facebook';
 export default class CommentsFB extends Component {
   render() {
     return (
-      <FacebookProvider appId="2172707499477897">
-        <Comments href="https://www.facebook.com/20531316728/posts/10154009990506729/" />
+      <FacebookProvider appId="681371895994182">
+        <Comments href="https://www.facebook.com/anitvdotga/posts/647123532410500/" />
       </FacebookProvider>
     );
   }
--- src/components/content-loader/content-loader.component.jsx
+++ src/components/content-loader/content-loader.component.jsx
@@ -1,39 +1,19 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
 
-const MyLoader = () => {
+const MyLoader = props => {
   return (
     <ContentLoader
-      speed={3}
-      width={1280}
-      height={980}
-      viewBox="0 0 1280 980"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#bfbfbf"
+      viewBox="0 0 400 160"
+      height={160}
+      width={400}
+      speed={2}
+      backgroundColor="transparent"
+      {...props}
     >
-      <rect x="880" y="25" rx="3" ry="3" width="70" height="20" />
-      <rect x="780" y="25" rx="3" ry="3" width="70" height="20" />
-      <rect x="81" y="986" rx="3" ry="3" width="1280" height="10" />
-      <rect x="81" y="1026" rx="3" ry="3" width="1080" height="9" />
-      <rect x="81" y="1046" rx="3" ry="3" width="980" height="8" />
-      <circle cx="1166" cy="38" r="16" />
-      <rect x="461" y="410" rx="0" ry="0" width="0" height="1" />
-      <rect x="90" y="88" rx="0" ry="0" width="1080" height="240" />
-      <rect x="680" y="24" rx="3" ry="3" width="70" height="20" />
-      <rect x="980" y="25" rx="3" ry="3" width="70" height="20" />
-      <rect x="88" y="18" rx="0" ry="0" width="100" height="50" />
-      <rect x="1080" y="12" rx="0" ry="0" width="44" height="44" />
-      <rect x="200" y="440" rx="0" ry="0" width="180" height="222" />
-      <rect x="120" y="380" rx="0" ry="0" width="120" height="28" />
-      <rect x="420" y="440" rx="0" ry="0" width="180" height="222" />
-      <rect x="640" y="440" rx="0" ry="0" width="180" height="222" />
-      <rect x="860" y="440" rx="0" ry="0" width="180" height="222" />
-      <rect x="461" y="734" rx="0" ry="0" width="0" height="1" />
-      <rect x="200" y="760" rx="0" ry="0" width="180" height="222" />
-      <rect x="120" y="700" rx="0" ry="0" width="120" height="28" />
-      <rect x="420" y="760" rx="0" ry="0" width="180" height="222" />
-      <rect x="640" y="760" rx="0" ry="0" width="180" height="222" />
-      <rect x="860" y="760" rx="0" ry="0" width="180" height="222" />
+      <circle cx="150" cy="86" r="10" />
+      <circle cx="194" cy="86" r="10" />
+      <circle cx="238" cy="86" r="10" />
     </ContentLoader>
   );
 };
